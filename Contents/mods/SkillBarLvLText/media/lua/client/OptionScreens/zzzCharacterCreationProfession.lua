33a04d7285c9e4e755f8def3417fbad6f3489978
Stefano Scalfari <vorshim92@gmail.com>
2025-01-31 12:25:50 +0100

    first commit

---
+++ Contents/mods/SkillBarLvLText/media/lua/client/OptionScreens/zzzCharacterCreationProfession.lua
@@ -0,0 +1,18 @@
+local vanilla_drawXp = CharacterCreationProfession.drawXpBoostMap
+
+function CharacterCreationProfession:drawXpBoostMap(y, item, alt)
+    local newY = vanilla_drawXp(self, y, item, alt)
+
+    local dy = (self.itemheight - self.fontHgt) / 2
+    local hc = getCore():getGoodHighlitedColor()
+    local lvlWidth = getTextManager():MeasureStringX(UIFont.Small, "Lvl "..item.item.level)
+    
+    local yForText = newY - self.itemheight -- per esempio
+
+    self:drawText("Lvl "..item.item.level,
+                  self.width - (68 + 10 * 4) - lvlWidth - 5,
+                  yForText + dy,
+                  hc:getR(), hc:getG(), hc:getB(), 1, UIFont.Small)
+
+    return newY
+end
