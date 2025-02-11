3ee055f30ead1f946aa7cf7c9094e0c47fab7cdb
Stefano Scalfari <vorshim92@gmail.com>
2025-02-11 21:58:57 +0100

    fix for b42

--- Contents/mods/SkillBarLvLText/common/media/lua/client/OptionScreens/zzzCharacterCreationProfession.lua
+++ Contents/mods/SkillBarLvLText/common/media/lua/client/OptionScreens/zzzCharacterCreationProfession.lua
@@ -2,17 +2,23 @@ local vanilla_drawXp = CharacterCreationProfession.drawXpBoostMap
 
 function CharacterCreationProfession:drawXpBoostMap(y, item, alt)
     local newY = vanilla_drawXp(self, y, item, alt)
+    local scrollBarWidth = 13
+    local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
 
     local dy = (self.itemheight - self.fontHgt) / 2
     local hc = getCore():getGoodHighlitedColor()
-    local lvlWidth = getTextManager():MeasureStringX(UIFont.Small, "Lvl "..item.item.level)
-    
-    local yForText = newY - self.itemheight
+    local blitH = FONT_HGT_SMALL
+    local blitW = math.floor(blitH / (10/3))
+    local blitGap = math.floor(blitW/4)
+    local blitXOffset = getTextManager():MeasureStringX(UIFont.Small, "+ 100%")+scrollBarWidth
+    local greenBlitsX = self.width - (blitXOffset + 12 * (blitW+blitGap))
+    local yy = y
 
-    self:drawText("Lvl "..item.item.level,
-                  self.width - (68 + 10 * 4) - lvlWidth - 5,
-                  yForText + dy,
-                  hc:getR(), hc:getG(), hc:getB(), 1, UIFont.Small)
+
+    -- Nuovo codice per il testo del livello
+    local levelText = "Lvl "..tostring(item.item.level)
+    local textWidth = getTextManager():MeasureStringX(UIFont.Small, levelText)
+    self:drawText(levelText, greenBlitsX - textWidth - blitGap, yy + dy, hc:getR(), hc:getG(), hc:getB(), 1, UIFont.Small)
 
     return newY
 end
