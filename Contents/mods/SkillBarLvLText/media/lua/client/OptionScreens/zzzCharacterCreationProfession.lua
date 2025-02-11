37f385fcab98cc60b84276c56e527003232b4c37
Stefano Scalfari <vorshim92@gmail.com>
2025-02-11 22:33:28 +0100

    update also for b41

--- Contents/mods/SkillBarLvLText/media/lua/client/OptionScreens/zzzCharacterCreationProfession.lua
+++ Contents/mods/SkillBarLvLText/media/lua/client/OptionScreens/zzzCharacterCreationProfession.lua
@@ -23,27 +23,29 @@ CharacterCreationProfession.drawXpBoostMap = function(self, y, item, alt)
     -- 5) Rimetto tutto a posto
     self.drawTexture = old_drawTexture
     local scrollBarWidth = 13
+    local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
+
     local dy = (self.itemheight - self.fontHgt) / 2
     local hc = getCore():getGoodHighlitedColor()
-    local lvlWidth = getTextManager():MeasureStringX(UIFont.Small, "Lvl "..item.item.level)
-    local yForText = newY - self.itemheight
-
-    local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
     local blitH = FONT_HGT_SMALL
     local blitW = math.floor(blitH / (10/3))
     local blitGap = math.floor(blitW/4)
     local blitXOffset = getTextManager():MeasureStringX(UIFont.Small, "+ 100%")+scrollBarWidth
     local greenBlitsX = self.width - (blitXOffset + 12 * (blitW+blitGap))
     local yy = y
+    local textWid = getTextManager():MeasureStringX(UIFont.Small, item.text)
+    if 16 + textWid > greenBlitsX - 4 then
+        yy = y + self.fontHgt
+    end
 
     for i = 1,item.item.level do
         self:drawTextureScaled(CharacterCreationProfession.instance.whiteBar, greenBlitsX+(i * (blitW+blitGap)), (yy) + dy, blitW, blitH, 1, hc:getR(), hc:getG(), hc:getB())
     end
 
-    self:drawText("Lvl "..item.item.level,
-                  self.width - (68 + 10 * 4) - lvlWidth - 5,
-                  yForText + dy,
-                  hc:getR(), hc:getG(), hc:getB(), 1, UIFont.Small)
+    -- Nuovo codice per il testo del livello
+    local levelText = "Lvl "..tostring(item.item.level)
+    local textWidth = getTextManager():MeasureStringX(UIFont.Small, levelText)
+    self:drawText(levelText, greenBlitsX - textWidth - blitGap, yy + dy, hc:getR(), hc:getG(), hc:getB(), 1, UIFont.Small)
     -- 6) Restituisco il risultato
     return newY
 end
